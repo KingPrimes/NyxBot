23da6b098ace7cac6d0b9d37d3000ee0b2ce0102
KingPrimes <15066650082@163.com>
2023-08-01 18:40:42 +0800

    增加机器人上下线提醒

---
+++ src/main/java/com/nyx/bot/custom/BotCoreEvent.java
@@ -0,0 +1,28 @@
+package com.nyx.bot.custom;
+
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.core.CoreEvent;
+import com.nyx.bot.utils.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Primary
+@Component
+public class BotCoreEvent extends CoreEvent {
+
+    @Override
+    public void online(Bot bot) {
+        log.info("已建立链接，BotId：{} \t 上线时间：{}",bot.getLoginInfo().getData().getUserId(), DateUtils.format(new Date()));
+    }
+
+    @Override
+    public void offline(long account) {
+        log.info("机器人 {} 已断开链接",account);
+    }
+
+
+}
