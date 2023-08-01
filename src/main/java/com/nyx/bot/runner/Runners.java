6d77bfc6b49c7834055a7496fb67f2f83340e01c
KingPrimes <15066650082@163.com>
2023-08-01 17:51:30 +0800

    增加Test属性

--- src/main/java/com/nyx/bot/runner/Runners.java
+++ src/main/java/com/nyx/bot/runner/Runners.java
@@ -1,6 +1,7 @@
 package com.nyx.bot.runner;
 
 import com.nyx.bot.utils.IoUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class Runners {
 
+    @Value("${test.isTest}")
+    Boolean test;
+
     //程序启动完成启动浏览器
     @Bean
     public ApplicationRunner browser(){
         return args -> {
-            IoUtils.index();
+            if(!test){
+                IoUtils.index();
+            }
         };
     }
 
