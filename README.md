f224d544daae792392f568824cb52910a95fd9f1
KingPrimes <15066650082@163.com>
2023-08-01 14:20:10 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# NyxBot
+<div align="center">
+
+![](.github/image/index.png)
+
+# NYXBot
+
+----
+
+
+</div>
---
+++ src/main/java/com/nyx/bot/NxyBotApplication.java
@@ -0,0 +1,13 @@
+package com.nyx.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NxyBotApplication.class, args);
+    }
+
+}
