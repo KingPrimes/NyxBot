8a83d291a1210b55b5d572665198ba51c3f2bbb2
KingPrimes <15066650082@163.com>
2023-08-02 00:57:31 +0800

    更改工具类

--- src/main/java/com/nyx/bot/utils/DateUtils.java
+++ src/main/java/com/nyx/bot/utils/DateUtils.java
@@ -1,19 +1,97 @@
 package com.nyx.bot.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
-public class DateUtils {
+public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
+
+    private static final String[] parsePatterns = {
+            "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM",
+            "yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM",
+            "yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm", "yyyy.MM"};
+
+    public static String yyyy = "yyyy-MM-dd HH:mm:ss";
+    public static String MM = "MM-dd HH:mm:ss";
+    public static String dd = "dd HH:mm:ss";
+    public static String hh = "HH:mm:ss";
+    public static String mm = "mm:ss";
+
 
     /**
      * 格式化日期 以 yyyy-MM-dd HH:mm:ss 格式
+     *
      * @param date 日期
      * @return 格式化之后的字符串日期
      */
-    public static String format(Date date){
-        String formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").toString();
-        return new SimpleDateFormat(formatter).format(date);
+    public static String format(Date date, String format) {
+        return new SimpleDateFormat(format).format(date);
     }
+
+
+    /**
+     * 取两个日期的时间差
+     *
+     * @param endDate   结束日期
+     * @param startDate 开始日期
+     * @return 动态返回计算结果
+     */
+    public static String getDiff(Date endDate, Date startDate) {
+        long yy = 1000L * 60 * 60 * 24 * 365;
+        long mm = 1000L * 60 * 60 * 24 * 30;
+        long nd = 1000 * 24 * 60 * 60;
+        long nh = 1000 * 60 * 60;
+        long nm = 1000 * 60;
+        long ns = 1000;
+        // 获得两个时间的毫秒时间差异
+        long diff = endDate.getTime() - startDate.getTime();
+        //计算差多少年
+        long years = diff / yy;
+        //计算差多少月
+        long diffSeconds = diff % yy / mm;
+        // 计算差多少天
+        long days = diff % yy % mm / nd;
+        // 计算差多少小时
+        long hours = diff % nd / nh;
+        // 计算差多少分钟
+        long minutes = diff % nd % nh / nm;
+        // 计算差多少秒//输出结果
+        long sec = diff % nd % nh % nm / ns;
+
+        //根据相差的时间返回不同的格式
+        if (years != 0) {
+            return years + "年" + diffSeconds + "月" + days + "日" + hours + "时" + minutes + "分" + sec + "秒";
+        }
+        if (diffSeconds != 0) {
+            return diffSeconds + "月" + days + "日" + hours + "时" + minutes + "分" + sec + "秒";
+        }
+        if (days != 0) {
+            return days + "日" + hours + "时" + minutes + "分" + sec + "秒";
+        }
+        if (hours != 0) {
+            return hours + "时" + minutes + "分" + sec + "秒";
+        }
+        if (minutes != 0) {
+            return minutes + "分" + sec + "秒";
+        }else  {
+            return sec + "秒";
+        }
+    }
+
+    /**
+     * 日期型字符串转化为日期 格式
+     */
+    public static Date parseDate(Object str) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            return parseDate(str.toString(), parsePatterns);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
 
 }
--- src/main/java/com/nyx/bot/utils/MatcherUtils.java
+++ src/main/java/com/nyx/bot/utils/MatcherUtils.java
@@ -16,4 +16,92 @@ public class MatcherUtils {
         Matcher m = p.matcher(str);
         return m.find();
     }
+
+    /**
+     * 判断字符串是否为URL
+     *
+     * @param urls 需要判断的String类型url
+     * @return true:是URL；false:不是URL
+     */
+    public static boolean isHttpUrl(String urls) {
+        boolean distro = false;
+        String regex = "((https|http)?://)";//设置正则表达式
+
+        Pattern pat = Pattern.compile(regex.trim());//对比
+        Matcher mat = pat.matcher(urls.trim());
+        while (mat.find()) {
+            distro = true;
+        }
+        return distro;
+    }
+
+    /**
+     * 判断是否是数字
+     *
+     * @param str 字符串
+     * @return true 是数字 反之
+     */
+    public static boolean isNumberAndDouble(String str) {
+        //是否整数
+        Pattern num = Pattern.compile("^\\d+$|-\\d+$");
+        //小数
+        Pattern dou = Pattern.compile("\\d+\\.\\d+$|-\\d+\\.\\d+$");
+
+        return num.matcher(str).matches()
+                || dou.matcher(str).matches();
+    }
+
+    /**
+     * 判断是否是整数
+     *
+     * @param str 字符串
+     * @return true 整数 反之
+     */
+    public static boolean isNumber(String str) {
+        //是否整数
+        Pattern num = Pattern.compile("^\\d+$|-\\d+$");
+        return num.matcher(str).matches();
+    }
+
+    /**
+     * 判断是否是字母
+     *
+     * @param str 字符串
+     * @return true 是字母 反之
+     */
+    public static boolean isAlpha(String str) {
+        return str != null && str.matches("[a-zA-z]+");
+    }
+
+    /**
+     * 全局匹配正则表达式
+     *
+     * @param str   字符串
+     * @param regex 正则表达式
+     * @return
+     */
+    public static boolean regexG(String str, String regex) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        while (m.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 正则表达式字符串替换
+     *
+     * @param str       字符串
+     * @param regex     正则表达式
+     * @param newString 新的替换字符串
+     * @return 返回替换后的字符串
+     */
+    public static String regReplace(String str, String regex, String newString) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        return m.replaceAll(newString);
+    }
+
+
 }
