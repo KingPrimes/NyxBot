1d0665377bba547e05c26b95bf10ff09d6403042
KingPrimes <15066650082@163.com>
2023-08-06 00:02:03 +0800

    修改初始化配置

--- src/main/java/com/nyx/bot/runner/Runners.java
+++ src/main/java/com/nyx/bot/runner/Runners.java
@@ -2,15 +2,16 @@ package com.nyx.bot.runner;
 
 import com.nyx.bot.data.WarframeDataSource;
 import com.nyx.bot.entity.SysUser;
+import com.nyx.bot.enums.AsyncBeanName;
 import com.nyx.bot.repo.SysUserRepository;
+import com.nyx.bot.utils.AsyncUtils;
 import com.nyx.bot.utils.IoUtils;
+import com.nyx.bot.plugin.warframe.WarframeSocket;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -40,10 +41,8 @@ public class Runners {
     //程序启动完成后初始化Web系统用户
     @Bean
     public ApplicationRunner sysUser() {
-        return new ApplicationRunner() {
-            @Async("myAsync")
-            @Override
-            public void run(ApplicationArguments args) {
+        return args -> {
+            AsyncUtils.me().execute(()->{
                 SysUser user = new SysUser();
                 user.setUserId(1L);
                 user.setUserName("admin");
@@ -53,7 +52,7 @@ public class Runners {
                     SysUser save = userRepository.save(user);
                     log.info("用户不存在！已添加新用户：{}",save);
                 }
-            }
+            },AsyncBeanName.SERVICE);
         };
     }
 
@@ -61,11 +60,19 @@ public class Runners {
     //程序启动完成后初始化数据
     @Bean
     public ApplicationRunner inItDataSource() {
-        return new ApplicationRunner() {
-            @Async("myAsync")
-            @Override
-            public void run(ApplicationArguments args) {
-                new WarframeDataSource().init();
+        return args -> {
+            if(!test) {
+                AsyncUtils.me().execute(WarframeDataSource::init);
+            }
+        };
+    }
+
+    @Bean
+    public ApplicationRunner warframeDataSocket(){
+        return args -> {
+            if(!test){
+                //创建WebSocket链接 或许游戏数据
+                WarframeSocket.socket().connectServer("ws://api.warframestat.us/socket");
             }
         };
     }
