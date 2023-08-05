b69c758a8cacf8973faedd5a05186ef9e615872c
KingPrimes <15066650082@163.com>
2023-08-06 00:04:56 +0800

    增加订阅列表

---
+++ src/main/java/com/nyx/bot/enums/SubscribeEnums.java
@@ -0,0 +1,67 @@
+package com.nyx.bot.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum SubscribeEnums {
+    /**
+     * 订阅所有内容
+     */
+    ERROR("没有此数值！"),
+    /**
+     * 警报
+     */
+    ALERTS("警报"),
+    /**
+     * 仲裁
+     */
+    ARBITRATION("仲裁"),
+    /**
+     * 夜灵平野
+     */
+    CETUS_CYCLE("夜灵平野"),
+    /**
+     * 每日特惠
+     */
+    DAILY_DEALS("每日特惠"),
+    /**
+     * 活动
+     */
+    EVENTS("活动"),
+    /**
+     * 入侵
+     */
+    INVASIONS("入侵"),
+    /**
+     * 钢铁兑换
+     */
+    STEEL_PATH("钢铁兑换"),
+    /**
+     * 奸商
+     */
+    VOID("奸商"),
+    /**
+     * 裂隙
+     */
+    FISSURES("裂隙"),
+    /**
+     * 新闻
+     */
+    NEWS("新闻"),
+    /**
+     * 电波
+     */
+    NIGHTWAVE("电波"),
+    /**
+     * 突击
+     */
+    SORTIE("突击"),
+
+    ;
+    private final String name;
+
+    SubscribeEnums(String name) {
+        this.name = name;
+    }
+
+}
