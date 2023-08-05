95f2beeff9d1ed753fc3282b375ad83d8005adbd
KingPrimes <15066650082@163.com>
2023-08-06 00:04:40 +0800

    增加注释

--- src/main/java/com/nyx/bot/enums/Permissions.java
+++ src/main/java/com/nyx/bot/enums/Permissions.java
@@ -4,7 +4,14 @@ package com.nyx.bot.enums;
  * 权限等级
  */
 public enum Permissions{
+    //其它用户
+    OTHER,
+    //普通用户
     USER,
+    //管理员用户
     ADMIN,
+    //系统管理员用户
     SUPER_ADMIN,
+    //后台用户
+    MANAGE,
 }
