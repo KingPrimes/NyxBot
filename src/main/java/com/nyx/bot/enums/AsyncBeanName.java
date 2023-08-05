39f561bb436d809c681d1b0883adaff8ee11fe9b
KingPrimes <15066650082@163.com>
2023-08-06 00:04:09 +0800

    用于获取异步BeanName

---
+++ src/main/java/com/nyx/bot/enums/AsyncBeanName.java
@@ -0,0 +1,20 @@
+package com.nyx.bot.enums;
+
+import lombok.Getter;
+
+
+/**
+ * 用于切换线程池
+ */
+@Getter
+public enum AsyncBeanName {
+
+    MYASYNC("myAsync"),
+    SERVICE("scheduledExecutorService"),
+    ;
+    private final String str;
+
+    AsyncBeanName(String s) {
+        str = s;
+    }
+}
