e676164b0fe7b56e36264b657e84a03a8ea54aa2
KingPrimes <15066650082@163.com>
2023-08-06 00:04:31 +0800

    执行的请求

---
+++ src/main/java/com/nyx/bot/enums/BusinessType.java
@@ -0,0 +1,57 @@
+package com.nyx.bot.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum BusinessType {
+    /**
+     * 其它
+     */
+    OTHER("其它"),
+
+    /**
+     * 查询
+     */
+    SELECT("查询"),
+
+    /**
+     * 新增
+     */
+    INSERT("新增"),
+
+    /**
+     * 修改
+     */
+    UPDATE("修改"),
+
+    /**
+     * 删除
+     */
+    DELETE("删除"),
+
+    /**
+     * 导出
+     */
+    EXPORT("导出"),
+
+    /**
+     * 导入
+     */
+    IMPORT("导入"),
+
+    /**
+     * 清空
+     */
+    CLEAN("清空"),
+    /**
+     * 生成图片
+     */
+    IMAGE("生成图片"),
+    ;
+    final String type;
+
+    BusinessType(String type) {
+        this.type = type;
+    }
+
+}
