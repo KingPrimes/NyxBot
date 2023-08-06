8279f40127169f713f7f2e3d5edd7b3a8a74cfc2
KingPrimes <15066650082@163.com>
2023-08-07 00:27:00 +0800

    增加Cache缓存管理器配置，交给SpringBoot管理

---
+++ src/main/java/com/nyx/bot/config/CachingConfig.java
@@ -0,0 +1,31 @@
+package com.nyx.bot.config;
+
+import org.cache2k.extra.spring.SpringCache2kCacheManager;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+
+    /***
+     * 配置Cache缓存管理器 交给Spring Boot管理
+     * @return 缓存管理器
+     */
+    @Bean
+    public CacheManager cacheManager() {
+        return new SpringCache2kCacheManager()
+                .addCaches(
+                        //配置名称为 warframe-socket-data 的缓存策略， eternal永不过期， entryCapacity 可以有多少个缓存
+                        b->b.name("warframe-socket-data").eternal(true).entryCapacity(1),
+                        //配置名称为 group-captcha 的缓存策略，expireAfterWrite设置过期时间，entryCapacity 可以有多少个缓存
+                        b->b.name("group-captcha").expireAfterWrite(30, TimeUnit.SECONDS).entryCapacity(10000),
+                        //配置名称为 system 的缓存策略，permitNullValues允许为空值，entryCapacity 可以有多少个缓存
+                        b->b.name("system").permitNullValues(true).entryCapacity(100));
+    }
+
+}
