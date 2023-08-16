ca93722d4f5ab9edc4f73128912aa3b65ce40132
KingPrimes <15066650082@163.com>
2023-08-16 21:17:46 +0800

    ThymeleafConfig配置

---
+++ src/main/java/com/nyx/bot/config/ThymeleafConfig.java
@@ -0,0 +1,24 @@
+package com.nyx.bot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver springResourceTemplateResolver(){
+        SpringResourceTemplateResolver srtr = new SpringResourceTemplateResolver();
+        srtr.setPrefix("file:./templates/");
+        srtr.setSuffix(".html");
+        srtr.setTemplateMode(TemplateMode.HTML);
+        srtr.setCharacterEncoding("UTF-8");
+        srtr.setOrder(1);
+        srtr.setCacheable(false);
+        srtr.setCheckExistence(true);
+        return srtr;
+    }
+
+}
