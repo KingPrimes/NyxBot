da85166c840cff4bbe4b356f866044e65d8a7faf
KingPrimes <15066650082@163.com>
2023-08-26 23:22:29 +0800

    更改目录

---
+++ src/main/java/com/nyx/bot/repo/sys/LogInfoRepository.java
@@ -0,0 +1,13 @@
+package com.nyx.bot.repo.sys;
+
+import com.nyx.bot.entity.sys.LogInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 日志信息
+ */
+@Repository
+public interface LogInfoRepository extends JpaRepository<LogInfo,Long>, JpaSpecificationExecutor<LogInfo> {
+}
---
+++ src/main/java/com/nyx/bot/repo/warframe/WeaponsRepository.java
@@ -0,0 +1,24 @@
+package com.nyx.bot.repo.warframe;
+
+import com.nyx.bot.entity.warframe.Weapons;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface WeaponsRepository extends JpaRepository<Weapons,Long> {
+    /**
+     * 添加数据
+     */
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT IGNORE INTO WEAPONS(icon, icon_for_mat, item_name, thumb, url_name, weapon_id) VALUES (:#{#weapon.icon},:#{#weapon.iconForMat},:#{#weapon.itemName},:#{#weapon.thumb},:#{#weapon.urlName},:#{#weapon.weaponId})",nativeQuery = true)
+    Integer addWeapons(@Param("weapon") Weapons weapons);
+
+    @Query(value = "select max(ID) from WEAPONS",nativeQuery = true)
+    Integer queryMaxId();
+
+}
