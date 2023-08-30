fdf6a23172ef4821945ad0fed22eb077ce54357a
KingPrimes <15066650082@163.com>
2023-08-30 21:06:49 +0800

    移除AnyMessAgeEvent线程池

--- src/main/java/com/nyx/bot/config/ExecutorConfig.java
+++ src/main/java/com/nyx/bot/config/ExecutorConfig.java
@@ -1,6 +1,5 @@
 package com.nyx.bot.config;
 
-import com.nyx.bot.enums.AsyncBeanName;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Slf4j
@@ -19,53 +17,32 @@ public class ExecutorConfig {
     @Value("${async.executor.thread.core_pool_size}")
     private int corePoolSize = 5;
     @Value("${async.executor.thread.max_pool_size}")
-    private int maxPoolSize = 5;
+    private int maxPoolSize = 20;
     @Value("${async.executor.thread.queue_capacity}")
     private int queueCapacity = 80;
     @Value("${async.executor.thread.name.prefix}")
     private String namePrefix = "async-";
 
     @Bean(name = "myAsync")
-    public Executor asyncServiceExecutor() {
+    public ThreadPoolTaskExecutor asyncServiceExecutor() {
         log.info("start asyncServiceExecutor");
         return executor(namePrefix);
     }
 
     @Bean
-    public Executor scheduledExecutorService() {
+    public ThreadPoolTaskExecutor scheduledExecutorService() {
         log.info("start scheduledExecutor");
         return executor("scheduled-");
     }
 
     @Bean
-    public Executor taskExecutor(){
+    public ThreadPoolTaskExecutor taskExecutor(){
         log.info("start taskExecutor");
         return executor("taskExecutor-");
     }
 
-    @Bean(name = "AnyMessageEvent")
-    public Executor anyMessageEvent() {
-        log.info("start AnyMessageEventExecutor");
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        //配置核心线程数
-        executor.setCorePoolSize(20);
-        //配置最大线程数
-        executor.setMaxPoolSize(50);
-        //配置队列大小
-        executor.setQueueCapacity(120);
-        //配置线程池中的线程的名称前缀
-        executor.setThreadNamePrefix("AnyMessageEvent-");
 
-        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        //执行初始化
-        executor.initialize();
-        return executor;
-    }
-
-
-    private Executor executor(String namePrefix){
+    private ThreadPoolTaskExecutor executor(String namePrefix){
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         //配置核心线程数
         executor.setCorePoolSize(corePoolSize);
