859c3835d79b8592ba384e86017e0c091d056462
KingPrimes <15066650082@163.com>
2023-08-30 21:18:00 +0800

    任务订阅更新修复 Null Bug

--- src/main/java/com/nyx/bot/plugin/warframe/utils/WarframeSubscribe.java
+++ src/main/java/com/nyx/bot/plugin/warframe/utils/WarframeSubscribe.java
@@ -1,5 +1,6 @@
 package com.nyx.bot.plugin.warframe.utils;
 
+import com.nyx.bot.utils.CacheUtils;
 import com.nyx.bot.res.GlobalStates;
 import com.nyx.bot.res.SocketGlobalStates;
 import com.nyx.bot.utils.DateUtils;
@@ -14,51 +15,53 @@ import java.util.Optional;
 public class WarframeSubscribe {
 
     public static void isUpdated(SocketGlobalStates states) {
+        //判断是否传来的空值 如果是空则不做一下运算
+        if (states == null) {
+            return;
+        }
+        try {
+            SocketGlobalStates d = CacheUtils.getGlobalState();
+            Optional.of(d).ifPresentOrElse(data -> {
 
+                GlobalStates form = states.getPacket().getData();
+                GlobalStates cacheData = data.getPacket().getData();
 
-        Cache cache = SpringUtils.getBean(CacheManager.class).getCache("warframe-socket-data");
-        SocketGlobalStates d = cache.get("data", SocketGlobalStates.class);
-        // 检查缓存内容是否为空
-        Optional.ofNullable(d).ifPresentOrElse(data->{
+                //检查仲裁信息是否为空值
+                Optional.ofNullable(form.getArbitration()).ifPresentOrElse(f -> {
 
-            GlobalStates form = states.getPacket().getData();
-            GlobalStates cacheData = data.getPacket().getData();
+                }, () -> {
+                    //为空则使用缓存中的仲裁信息覆盖
+                    states.getPacket().getData().setArbitration(cacheData.getArbitration());
+                });
 
-            //检查仲裁信息是否为空值
-            Optional.ofNullable(form.getArbitration()).ifPresentOrElse(f->{
+                //检查警报是否不为空
+                Optional.ofNullable(form.getAlerts()).ifPresent(f -> {
+                    //判断数据是否有更新
+                    if (f.equals(cacheData.getAlerts())) {
+                        WarframeDataUpdateMission.updateAlerts();
+                    }
+                });
 
-            },()->{
-                //为空则使用缓存中的仲裁信息覆盖
-                states.getPacket().getData().setArbitration(cacheData.getArbitration());
-            });
-
-            //检查警报是否不为空
-            Optional.ofNullable(form.getAlerts()).ifPresent(f->{
-                //判断数据是否有更新
-                if (f.equals(cacheData.getAlerts())) {
-                    WarframeDataUpdateMission.updateAlerts();
-                }
-            });
-
-            //检查 夜灵平野 是否为空
-            Optional.ofNullable(form.getCetusCycle()).ifPresent(f->{
-                //判断是否更新
-                if (form.getCetusCycle().getState().toLowerCase(Locale.ROOT).equals("day")) {
-                    if (DateUtils.getDateHour(form.getCetusCycle().getExpiry(), new Date()) == 0) {
-                        long date = DateUtils.getDateMin(form.getCetusCycle().getExpiry(), new Date());
-                        if (date == 13) {
-                            WarframeDataUpdateMission.updateCetusCycle(DateUtils.getDiff(form.getCetusCycle().getExpiry(), new Date()));
+                //检查 夜灵平野 是否为空
+                Optional.ofNullable(form.getCetusCycle()).ifPresent(f -> {
+                    //判断是否更新
+                    if (form.getCetusCycle().getState().toLowerCase(Locale.ROOT).equals("day")) {
+                        if (DateUtils.getDateHour(form.getCetusCycle().getExpiry(), new Date()) == 0) {
+                            long date = DateUtils.getDateMin(form.getCetusCycle().getExpiry(), new Date());
+                            if (date == 13) {
+                                WarframeDataUpdateMission.updateCetusCycle(DateUtils.getDiff(form.getCetusCycle().getExpiry(), new Date()));
+                            }
                         }
                     }
-                }
+                });
+
+                CacheUtils.setGlobalState(states);
+            }, () -> {
+                CacheUtils.setGlobalState(states);
             });
+        } catch (Exception e) {
+            CacheUtils.setGlobalState(states);
+        }
 
-
-
-
-            cache.put("data",states);
-        },()->{
-            cache.put("data",states);
-        });
     }
 }
