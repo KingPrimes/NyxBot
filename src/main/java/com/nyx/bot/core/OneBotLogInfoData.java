1c5a8b10e484d6a67354b5ae55896f75fd4e6974
KingPrimes <15066650082@163.com>
2023-08-30 21:20:20 +0800

    更改OneBotLogInfoData 属性

--- src/main/java/com/nyx/bot/core/OneBotLogInfoData.java
+++ src/main/java/com/nyx/bot/core/OneBotLogInfoData.java
@@ -1,5 +1,6 @@
 package com.nyx.bot.core;
 
+import com.nyx.bot.enums.Codes;
 import com.nyx.bot.enums.PermissionsEnums;
 import lombok.Data;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -12,17 +13,37 @@ public class OneBotLogInfoData {
     Long groupUid;
     String rawMsg;
     String time;
+    //权限
     PermissionsEnums permissionsEnums;
+    //指令
+    Codes codes;
+
+    //Market平台
+    String form;
+
+    //key
+    String key;
+
+    //是否是买家
+    Boolean isBy;
+
+    //是否是满级
+    Boolean isMax;
 
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
-                .append("botUid", getBotUid())
-                .append("userUid", getUserUid())
-                .append("groupUid", getGroupUid())
-                .append("rawMsg", getRawMsg())
-                .append("time", getTime())
-                .append("permissionsEnums", getPermissionsEnums())
+                .append("botUid", botUid)
+                .append("userUid", userUid)
+                .append("groupUid", groupUid)
+                .append("rawMsg", rawMsg)
+                .append("time", time)
+                .append("permissionsEnums", permissionsEnums)
+                .append("codes", codes)
+                .append("form", form)
+                .append("key", key)
+                .append("isBy", isBy)
+                .append("isMax", isMax)
                 .toString();
     }
 }
