e29f91c455200c9d2b635e733e98f927e22be1aa
KingPrimes <15066650082@163.com>
2023-08-30 21:18:43 +0800

    HttpUtils 包装返回

--- src/main/java/com/nyx/bot/plugin/HelpCode.java
+++ src/main/java/com/nyx/bot/plugin/HelpCode.java
@@ -3,6 +3,8 @@ package com.nyx.bot.plugin;
 
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.AnyMessageEvent;
+import com.nyx.bot.enums.HttpCodeEnum;
+import com.nyx.bot.utils.HttpUtils;
 import com.nyx.bot.utils.onebot.ImageUrlUtils;
 import com.nyx.bot.utils.onebot.Msg;
 
@@ -11,12 +13,12 @@ public class HelpCode {
 
     //菜单，列出所有的指令与权限
     public static void help(Bot bot, AnyMessageEvent event) {
-        byte[] bytes = ImageUrlUtils.builderBase64Post(
+        HttpUtils.Body bytes = ImageUrlUtils.builderBase64Post(
                 "help",
                 bot, event);
-        if (bytes != null) {
+        if (bytes.getCode().equals(HttpCodeEnum.SUCCESS)) {
             bot.sendMsg(event,
-                    Msg.builder().imgBase64(bytes).build(), false);
+                    Msg.builder().imgBase64(bytes.getFile()).build(), false);
         }
 
     }
