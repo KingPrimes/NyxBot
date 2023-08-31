5cdf4e09c8775fed4163825900d6700bc24eabea
KingPrimes <15066650082@163.com>
2023-08-31 20:28:01 +0800

    更改Repository

--- src/main/java/com/nyx/bot/repo/warframe/OrdersItemsRepository.java
+++ src/main/java/com/nyx/bot/repo/warframe/OrdersItemsRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * OrdersItem 用于查询Warframe.Market 上面的物品,
  * 根据指令匹配本地数据
@@ -31,4 +33,33 @@ public interface OrdersItemsRepository extends JpaRepository<OrdersItems, Long>,
      * 查询最大ID的数据
      */
     OrdersItems findTopByOrderByIdDesc();
+
+
+    /**
+     * 根据物品名称模糊查询
+     *
+     * @param itemName 物品名称
+     * @return 结果
+     */
+    @Query(value = "select * from ORDERS_ITEMS where upper(replace(ITEM_NAME,' ','')) like upper(replace('%'||:#{#i}||'%',' ','')) and URL_NAME regexp '(.*set)?' group by ITEM_NAME limit 1", nativeQuery = true)
+    OrdersItems findByItemNameLike(@Param("i") String itemName);
+
+    /**
+     * 根据物品名称模糊查询
+     *
+     * @param itemName 物品名称
+     * @return 结果
+     */
+
+    @Query(value = "select * from ORDERS_ITEMS where upper(replace(ITEM_NAME,' ','')) like upper(replace('%'||:#{#i}||'%',' ',''))", nativeQuery = true)
+    List<OrdersItems> findByItemNameLikeToList(@Param("i") String itemName);
+
+    /**
+     * 正则查询
+     *
+     * @param regex 正则表达式
+     */
+    @Query(value = "select * from ORDERS_ITEMS where upper(replace(ITEM_NAME,' ','')) regexp upper(replace(:#{#r},' ',''))", nativeQuery = true)
+    OrdersItems findByItemNameRegex(@Param("r") String regex);
+
 }
