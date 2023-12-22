30c996433265475e30810df470863d8fe57f0de1
KingPrimes <Sakitama_q@163.com>
2023-12-22 17:10:54 +0800

    修改HttpUtils类 - 增加超时时间 - 忽略SSL验证

--- src/main/java/com/nyx/bot/utils/HttpUtils.java
+++ src/main/java/com/nyx/bot/utils/HttpUtils.java
@@ -5,10 +5,18 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 
+import javax.net.ssl.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class HttpUtils {
@@ -21,7 +29,21 @@ public class HttpUtils {
     public static final MediaType MEDIA_TYPE_TEXT = MediaType.parse("text/plain");
     public static final String CONTENT_TYPE_FORM_DATA = "multipart/form-data";
 
-    private static final OkHttpClient client = new OkHttpClient();
+    private static final X509TrustManager manager = HttpUtils.getX509TrustManager();
+    private static final OkHttpClient client = new OkHttpClient().newBuilder()
+            //调用超时
+            .callTimeout(30, TimeUnit.SECONDS)
+            //链接超时
+            .connectTimeout(30, TimeUnit.SECONDS)
+            //读取超时
+            .readTimeout(30, TimeUnit.SECONDS)
+            //忽略SSL校验
+            .sslSocketFactory(HttpUtils.getSocketFactory(manager), manager)
+            //忽略校验
+            .hostnameVerifier(HttpUtils.getHostnameVerifier())
+            //连接池
+            .connectionPool(new ConnectionPool(10, 20, TimeUnit.MINUTES))
+            .build();
 
 
     public static Body sendGet(String url) {
@@ -29,11 +51,11 @@ public class HttpUtils {
     }
 
     public static Body sendGet(String url, String param) {
-        return sendGet(url, param, Headers.of("*", "*"));
+        return sendGet(url, param, Headers.of("*", "*").newBuilder());
     }
 
     public static Body sendGet(String url, Headers headers) {
-        return sendGet(url, "", headers);
+        return sendGet(url, "", headers.newBuilder());
     }
 
 
@@ -45,11 +67,14 @@ public class HttpUtils {
      * @param headers 请求头
      * @return 返回的文本
      */
-    public static Body sendGet(String url, String param, Headers headers) {
+    public static Body sendGet(String url, String param, Headers.Builder headers) {
         log.debug("发送请求\nUrl:{}\n参数:{}\n请求头:{}", url, param, headers.toString());
-        try (Response response = client.newCall(send(url, param, headers)).execute()) {
+        try (
+
+                Response response = client.newCall(send(url, param, headers)).execute()
+        ) {
             Body body = new Body();
-            Optional.ofNullable(response.body().string()).ifPresentOrElse(r -> {
+            Optional.of(response.body().string()).ifPresentOrElse(r -> {
                 body.setBody(r);
                 body.setCode(HttpCodeEnum.SUCCESS);
                 response.close();
@@ -59,11 +84,12 @@ public class HttpUtils {
 
             return body;
         } catch (IOException e) {
+            log.error(e.getMessage());
             return new Body(HttpCodeEnum.ERROR);
         }
     }
 
-    private static Request send(String url, String param, Headers headers) {
+    private static Request send(String url, String param, Headers.Builder headers) {
         String urlNameString;
         if (!param.isEmpty()) {
             urlNameString = url + "?" + param;
@@ -82,8 +108,35 @@ public class HttpUtils {
         return new Request.Builder()
                 .url(urlNameString)
                 .get()
-                .headers(headers)
+                .headers(headers.build())
                 .build();
+
+    }
+
+    /**
+     * 发送Get请求
+     *
+     * @param url     请求地址
+     * @param param   请求参数 key=v&key=v
+     * @param headers 请求头
+     * @return 返回值
+     */
+    public static String sendGetOkHttp(String url, String param, Headers.Builder headers) {
+        try {
+            Call call = new OkHttpClient().newCall(send(url, param, headers));
+            call.timeout().timeout(30, TimeUnit.SECONDS);
+            Response response = call.execute();
+            String tmp = Objects.requireNonNull(response.body()).string();
+            response.close();
+            return tmp;
+        } catch (Exception e) {
+            if (e.getMessage().equals("timeout")) {
+                log.error("Url:{} 请求超时", url + param);
+                return "timeout";
+            }
+            log.error("Url:{} \n\t\t错误信息:{}", url + param, e.getMessage());
+            return "";
+        }
 
     }
 
@@ -231,4 +284,41 @@ public class HttpUtils {
             this.code = code;
         }
     }
+
+    private static SSLSocketFactory getSocketFactory(TrustManager manager) {
+        SSLSocketFactory factory = null;
+        try {
+            SSLContext context = SSLContext.getInstance("SSL");
+            context.init(null, new TrustManager[]{manager}, new SecureRandom());
+            factory = context.getSocketFactory();
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+        return factory;
+    }
+
+    public static X509TrustManager getX509TrustManager() {
+        return new X509TrustManager() {
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        };
+    }
+
+
+    private static HostnameVerifier getHostnameVerifier() {
+        return (s, sslSession) -> true;
+    }
+
 }
