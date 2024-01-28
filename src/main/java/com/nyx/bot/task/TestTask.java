ec4d2a4a58647605ab17dd79c147e65cd650d752
KingPrimes <Sakitama_q@163.com>
2024-01-28 22:33:22 +0800

    增加定时任务

---
+++ src/main/java/com/nyx/bot/task/TestTask.java
@@ -0,0 +1,29 @@
+package com.nyx.bot.task;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
+import com.nyx.bot.core.ApiUrl;
+import com.nyx.bot.enums.HttpCodeEnum;
+import com.nyx.bot.plugin.warframe.utils.WarframeSubscribe;
+import com.nyx.bot.res.GlobalStates;
+import com.nyx.bot.utils.http.HttpUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class TestTask {
+
+    @Scheduled(cron = "0/60 * * * * ?")
+    public void execute() {
+        HttpUtils.Body body = HttpUtils.sendGet(ApiUrl.WARFRAME_STATUS + "pc");
+        if (body.getCode().equals(HttpCodeEnum.SUCCESS)) {
+            GlobalStates states = JSONObject.parseObject(body.getBody(), GlobalStates.class, JSONReader.Feature.SupportSmartMatch);
+            WarframeSubscribe.isUpdated(states);
+        } else {
+            log.info("获取数据失败！");
+        }
+
+    }
+}
