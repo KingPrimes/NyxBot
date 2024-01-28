97dadbc91528fcfc580da6ae261c5837d1a4744e
KingPrimes <Sakitama_q@163.com>
2024-01-28 22:38:56 +0800

    开启定时任务

--- src/main/java/com/nyx/bot/NxyBotApplication.java
+++ src/main/java/com/nyx/bot/NxyBotApplication.java
@@ -3,9 +3,11 @@ package com.nyx.bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class NxyBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(NxyBotApplication.class, args);
