a460ad2c2efecc7b35ae64d807b30c2e1c71ce05
KingPrimes <Sakitama_q@163.com>
2024-02-12 00:34:41 +0800

    File工具类

--- src/main/java/com/nyx/bot/utils/FileUtils.java
+++ src/main/java/com/nyx/bot/utils/FileUtils.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -22,15 +23,39 @@ public class FileUtils {
             inputStream.read(b);
             inputStream.close();
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error("read file error:{}", e.getMessage());
         }
-
         return new String(b, StandardCharsets.UTF_8);
     }
 
-    //写入文件
+    /**
+     * 写入文件
+     *
+     * @param fileName 文件路径+文件名字
+     * @param content  要写入的内容
+     */
     public static void writeFile(String fileName, String content) {
+        File file = new File(fileName);
+        try (FileOutputStream stream = new FileOutputStream(file)) {
+            stream.write(content.getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            log.error("write file error:{}", e.getMessage());
+        }
+    }
 
+    /**
+     * 写入文件
+     *
+     * @param fileName 文件路径+文件名字
+     * @param content  要写入的内容
+     */
+    public static void writeFile(String fileName, byte[] content) {
+        File file = new File(fileName);
+        try (FileOutputStream stream = new FileOutputStream(file)) {
+            stream.write(content);
+        } catch (Exception e) {
+            log.error("write file error:{}", e.getMessage());
+        }
     }
 
 }
