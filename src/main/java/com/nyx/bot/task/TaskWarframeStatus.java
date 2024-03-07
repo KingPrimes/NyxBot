1221acd575f5a0181b4c42088f50ad8ef5774d1d
KingPrimes <Sakitama_q@163.com>
2024-03-07 19:53:18 +0800

    更改数据获取方式为 Socket链接获取

--- src/main/java/com/nyx/bot/task/TaskWarframeStatus.java
+++ src/main/java/com/nyx/bot/task/TaskWarframeStatus.java
@@ -1,13 +1,6 @@
 package com.nyx.bot.task;
 
-import com.alibaba.fastjson2.JSONObject;
-import com.alibaba.fastjson2.JSONReader;
-import com.nyx.bot.core.ApiUrl;
-import com.nyx.bot.enums.HttpCodeEnum;
 import com.nyx.bot.plugin.warframe.utils.RivenDispositionUpdates;
-import com.nyx.bot.plugin.warframe.utils.WarframeSubscribe;
-import com.nyx.bot.res.GlobalStates;
-import com.nyx.bot.utils.http.HttpUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class TaskWarframeStatus {
 
-    @Scheduled(cron = "0/60 * * * * ?")
+   /* @Scheduled(cron = "0/60 * * * * ?")
     public void execute() {
         HttpUtils.Body body = HttpUtils.sendGet(ApiUrl.WARFRAME_STATUS + "pc");
         if (body.getCode().equals(HttpCodeEnum.SUCCESS)) {
@@ -25,7 +18,7 @@ public class TaskWarframeStatus {
         } else {
             log.info("获取数据失败！");
         }
-    }
+    }*/
 
     @Scheduled(cron = "0 0 0 1,11,21,31 * ? ")
     public void executeRivenTrend() {
