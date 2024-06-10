9b8b4d24dec5eb2f0954f506518eeb828b027412
KingPrimes <Sakitama_q@163.com>
2024-06-10 22:12:34 +0800

    feat(test):

---
+++ src/test/java/com/nyx/bot/NyxBotApplicationTest.java
@@ -0,0 +1,21 @@
+package com.nyx.bot;
+
+import com.nyx.bot.controller.config.bot.HandOff;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class NyxBotApplicationTest {
+    public static void main(String[] args) {
+        // 使用自定义的环境实例启动 Spring 应用
+        SpringApplication app = new SpringApplication(NyxBotApplicationTest.class);
+        app.setEnvironment(HandOff.getEnv());
+        app.run(args);
+    }
+
+
+}
