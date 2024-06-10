3c21527013e727f980c379875e28ee2f76f23667
KingPrimes <Sakitama_q@163.com>
2024-06-10 22:15:01 +0800

    style:

--- src/test/java/com/nyx/bot/plugin/warframe/utils/TestMarketUtils.java
+++ src/test/java/com/nyx/bot/plugin/warframe/utils/TestMarketUtils.java
@@ -1,17 +1,20 @@
 package com.nyx.bot.plugin.warframe.utils;
 
 import com.alibaba.fastjson2.JSON;
+import com.nyx.bot.NyxBotApplication;
 import com.nyx.bot.res.MarketRiven;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-/*@SpringBootTest(classes = NyxBotApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, useMainMethod = SpringBootTest.UseMainMethod.NEVER)*/
+@SpringBootTest(classes = NyxBotApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, useMainMethod = SpringBootTest.UseMainMethod.NEVER)
 @Slf4j
 public class TestMarketUtils {
 
     /**
      * 测试查询紫卡所需的时间
      */
-  /*  @Test*/
+    @Test
     public void testMarketRiven() {
         // 记录执行时间
         long start = System.currentTimeMillis();
