611ad6c74a85669ca928a2e141a31cb4e36e5d10
Jiuai <admin@9ikj.cn>
2024-06-28 02:23:38 +0800

    feat:因WFCD获取不到仲裁数据，增加临时仲裁数据（已确保数据暂时准确）

--- src/main/java/com/nyx/bot/controller/api/html/warframe/mission/ArbitrationHtmlController.java
+++ src/main/java/com/nyx/bot/controller/api/html/warframe/mission/ArbitrationHtmlController.java
@@ -1,10 +1,15 @@
 package com.nyx.bot.controller.api.html.warframe.mission;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONReader;
 import com.nyx.bot.exception.DataNotInfoException;
 import com.nyx.bot.repo.impl.warframe.TranslationService;
+import com.nyx.bot.res.ArbitrationPre;
 import com.nyx.bot.res.GlobalStates;
 import com.nyx.bot.utils.CacheUtils;
 import com.nyx.bot.utils.DateUtils;
+import com.nyx.bot.utils.ResourceUtil;
 import com.nyx.bot.utils.StringUtils;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
@@ -12,7 +17,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 仲裁
