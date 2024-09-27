2fad5a256d2bbe324b4830ab905fe2fd234d0f39
KingPrimes <15066650082@163.com>
2024-09-28 05:28:52 +0800

    perf(WarframeApi): 被动接受改为主动请求数据

--- src/main/java/com/nyx/bot/task/TaskWarframeStatus.java
+++ src/main/java/com/nyx/bot/task/TaskWarframeStatus.java
@@ -1,6 +1,13 @@
 package com.nyx.bot.task;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
+import com.nyx.bot.core.ApiUrl;
+import com.nyx.bot.enums.HttpCodeEnum;
 import com.nyx.bot.plugin.warframe.utils.RivenDispositionUpdates;
+import com.nyx.bot.plugin.warframe.utils.WarframeSubscribe;
+import com.nyx.bot.res.GlobalStates;
+import com.nyx.bot.utils.http.HttpUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,7 +17,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class TaskWarframeStatus {
 
-/*    @Async("taskExecutor")
+    @Async("taskExecutor")
     @Scheduled(cron = "0/60 * * * * ?")
     public void execute() {
         HttpUtils.Body body = HttpUtils.sendGet(ApiUrl.WARFRAME_STATUS + "pc");
@@ -20,7 +27,7 @@ public class TaskWarframeStatus {
         } else {
             log.info("获取数据失败！");
         }
-    }*/
+    }
 
     @Async("taskExecutor")
     @Scheduled(cron = "0 0 0 1,11,21,31 * ? ")
