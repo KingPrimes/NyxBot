cd426e0aa183bbf5878df90ee89bd72a0c67d3c5
KingPrimes <15066650082@163.com>
2024-12-26 02:20:31 +0800

    fix: 优化文件路径推送逻辑

--- src/main/java/com/nyx/bot/controller/data/warframe/TranslationController.java
+++ src/main/java/com/nyx/bot/controller/data/warframe/TranslationController.java
@@ -95,7 +95,7 @@ public class TranslationController extends BaseController {
             String jsonString = pushJson(all);
             FileUtils.writeFile(JgitUtil.lockPath + "/warframe/translation.json", jsonString);
             String branchName = DateUtils.getDate(new Date(), DateUtils.NOT_HMS);
-            build.pushBranchCheckout(commit, branchName, JgitUtil.lockPath + "/warframe/translation.json");
+            build.pushBranchCheckout(commit, branchName, "warframe/translation.json");
             return toAjax(true);
         } catch (Exception e) {
             throw new RuntimeException(e);
