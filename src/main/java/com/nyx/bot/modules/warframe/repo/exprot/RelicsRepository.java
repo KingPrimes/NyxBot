df6d32de6fa5af5f42c0e13975e94b35c310c099
KingPrimes <Sakitama_q@163.com>
2025-09-03 20:10:16 +0800

    fix(warframe): 优化遗物查询功能
    - 添加对 Forma 类遗物查询的限制
    - 修改查询方法以忽略大小写，提高查询准确性
    - 优化查询逻辑，先进行精确查询再进行模糊查询

--- src/main/java/com/nyx/bot/modules/warframe/repo/exprot/RelicsRepository.java
+++ src/main/java/com/nyx/bot/modules/warframe/repo/exprot/RelicsRepository.java
@@ -21,40 +21,44 @@ public interface RelicsRepository extends JpaRepository<Relics, String>, JpaSpec
      * @param rewardName 奖励名称
      */
 
-    List<Relics> findByRelicRewardsRewardNameContaining(String rewardName);
-
+    List<Relics> findByRelicRewardsRewardNameContainingIgnoreCase(String rewardName);
 
 
     /**
-     *  扩展：开头匹配（如"Prime%"）
+     * 扩展：开头匹配（如"Prime%"）
+     *
      * @param prefix 前缀
      */
     List<Relics> findByRelicRewardsRewardNameStartingWith(String prefix);
 
     /**
      * 扩展：结尾匹配（如"%Blueprint"）
+     *
      * @param suffix 后缀
      */
     List<Relics> findByRelicRewardsRewardNameEndingWith(String suffix);
 
     /**
      * 分页条件查询
+     *
      * @param re 条件
      * @param p  分页
      * @return Page<Relics>
      */
-    @Query("select r from Relics r where (:#{#re.name} is null or r.name = :#{#re.name})")
+    @Query("select r from Relics r where (:#{#re.name} is null or LOWER(r.name) = LOWER(:#{#re.name}))")
     Page<Relics> findAllPageable(@Param("re") Relics re, Pageable p);
 
     /**
      * 根据名称查询
+     *
      * @param name 遗物名称
      */
-    List<Relics> findByName(String name);
+    List<Relics> findByNameIgnoreCase(String name);
 
     /**
      * 模糊查询
+     *
      * @param name 遗物名称
      */
-    List<Relics> findByNameContaining(String name);
+    List<Relics> findByNameContainingIgnoreCase(String name);
 }
