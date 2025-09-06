0626ab441f2f6e43c3cb48aabfe0657d18b7ea4a
KingPrimes <Sakitama_q@163.com>
2025-09-06 23:39:31 +0800

    feat(aop): 添加插件异常处理切面
    - 新增 PluginHandlerException 类，用于捕获插件执行过程中的异常
    - 实现了日志记录和异常信息反馈的功能
    - 优化了日志输出，移除了冗余的指令使用日志
    - 重构了帮助插件的响应逻辑
    - 统一了指令执行成功和失败的日志格式
    - 修复了指令名称中的特殊字符问题

--- src/main/java/com/nyx/bot/utils/SendUtils.java
+++ src/main/java/com/nyx/bot/utils/SendUtils.java
@@ -11,15 +11,15 @@ public class SendUtils {
         if (imageBytes != null && imageBytes.length > 0) {
             // 直接使用字节数组构建图片消息
             bot.sendMsg(event, ArrayMsgUtils.builder().img(imageBytes).build(), false);
-            log.debug("群：{} 用户:{} 指令 {} 执行成功", event.getGroupId(), event.getUserId(), code.getComm());
+            log.debug("群：{} 用户:{} 指令 {} 执行成功", event.getGroupId(), event.getUserId(), StringUtils.removeMatcher(code.getComm()));
             return;
         }
         sendErrorMsg(bot, event, code);
-        log.debug("群：{} 用户:{} 指令 {} 执行失败", event.getGroupId(), event.getUserId(), code.getComm());
+        log.debug("群：{} 用户:{} 指令 {} 执行失败", event.getGroupId(), event.getUserId(), StringUtils.removeMatcher(code.getComm()));
     }
 
     public static void sendErrorMsg(Bot bot, AnyMessageEvent event, Codes codes) {
-        bot.sendMsg(event, codes.getComm() + " 执行失败", false);
+        bot.sendMsg(event, StringUtils.removeMatcher(codes.getComm()) + " 执行失败", false);
     }
 
     public static void not(Bot bot, AnyMessageEvent event) {
