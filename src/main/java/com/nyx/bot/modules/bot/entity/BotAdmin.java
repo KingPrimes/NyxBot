ca6069771872f201ce3374cc975d6d60228c69b1
KingPrimes <Sakitama_q@163.com>
2025-09-16 18:58:43 +0800

    feat(bot): 添加 QQ 号码有效性验证
    - 在 BotAdmin 实体类中添加了对 botUid 和 adminUid 的验证注解
    - 新增了 QQ 号码格式验证的国际化消息
    - 优化了消息属性文件的格式和内容

--- src/main/java/com/nyx/bot/modules/bot/entity/BotAdmin.java
+++ src/main/java/com/nyx/bot/modules/bot/entity/BotAdmin.java
@@ -7,6 +7,8 @@ import com.nyx.bot.common.core.Views;
 import com.nyx.bot.common.core.dao.BaseEntity;
 import com.nyx.bot.enums.PermissionsEnums;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -20,9 +22,16 @@ public class BotAdmin extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @NotEmpty(message = "bot.not.empty")
+    // qq号检查
+    @Min(value = 10000, message = "qq.uid.invalid") // 最小5位数字(10000)
+    @Digits(integer = 13, fraction = 0, message = "qq.uid.invalid") // 最大13位数字
     Long botUid;
+    // qq号检查
+    @Min(value = 10000, message = "qq.uid.invalid") // 最小5位数字(10000)
+    @Digits(integer = 13, fraction = 0, message = "qq.uid.invalid") // 最大13位数字
     @NotEmpty(message = "admin.not.empty")
     Long adminUid;
+
     PermissionsEnums permissions;
 
     @JsonIgnore
