34fbfa4f87a4699fa127db100db5cbe2457b394c
KingPrimes <Sakitama_q@163.com>
2025-09-19 02:55:45 +0800

    refactor(controller): 重构控制器包名并优化机器人服务异常处理 - 将 com.nyx.bot.modules.system.controller 改名为 com.nyx.bot.controller - 在 BotsService 中添加异常捕获，优化 getFriendList 和 getGroupList 方法的错误处理 - 新增错误消息，提示手动输入管理员账号或群账号

--- src/main/java/com/nyx/bot/controller/IndexController.java
+++ src/main/java/com/nyx/bot/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.nyx.bot.modules.system.controller;
+package com.nyx.bot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
