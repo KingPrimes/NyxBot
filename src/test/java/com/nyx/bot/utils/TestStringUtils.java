3f37203967f70eefa3c2a0501c45e8ec5ae15ca4
KingPrimes <Sakitama_q@163.com>
2025-11-21 20:53:28 +0800

    test: 移除所有测试类文件
    - 删除了多个与数据初始化、市场订单、夜灵平野等相关的测试类
    - 清理了绘图功能相关测试代码
    - 移除了模型测试和常量定义测试文件
    - 统一清理了冗余的测试资源文件引用

---
+++ src/test/java/com/nyx/bot/utils/TestStringUtils.java
@@ -0,0 +1,14 @@
+package com.nyx.bot.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class TestStringUtils {
+
+    @Test
+    void testGetWeaponsEnglishName() {
+        String description = "这些凝聚虚空能量的锋利之刃能造成了深切而痛苦的伤口。";
+        String substring = StringUtils.getSubString(description, "（英文：", "）");
+        String name = StringUtils.convertToCamelCase(substring);
+        System.out.println(name);
+    }
+}
